7719a0744cf07e685074ce1c3533cf6a2d92409d
unknown <chetansv.xworkz@gmail.com>
2022-09-15 08:37:36 +0530

    hi

---
+++ CapitalByState.java
@@ -0,0 +1,231 @@
+class CapitalByState {
+	static String getCapitalByState(String capital)
+	{
+		if (capital=="bangalore")
+		{	
+	System.out.println(capital);
+		System.out.println("is the capital city of the state");
+		return "Karanataka";	 // 1
+	}
+	if (capital=="Amaravati")
+		{	
+	System.out.println(capital);
+		System.out.println("is the capital city of the state");
+		return "Andhra Pradesh"; //2 	
+	}
+	if (capital=="Itanagar")
+		{	
+	System.out.println(capital);
+		System.out.println("is the capital city of the state");
+		return "Arunachal Pradesh"; ///3	
+	}
+	if (capital=="Dispur")
+		{	
+	System.out.println(capital);
+		System.out.println("is the capital city of the state");
+		return "Assam";	///4
+	}
+	if (capital=="Patna")
+		{	
+	System.out.println(capital);
+		System.out.println("is the capital city of the state");
+		return "Bihar";	///5
+	}
+	if (capital=="Raipur")
+		{	
+	System.out.println(capital);
+		System.out.println("is the capital city of the state");
+		return "Chhattisgarh";	///29
+	}
+	if (capital=="Panaji")
+		{	
+	System.out.println(capital);
+		System.out.println("is the capital city of the state");
+		return "Goa";	//6
+	}
+	if (capital=="Gandhinagar")
+		{	
+	System.out.println(capital);
+		System.out.println("is the capital city of the state");
+		return "Gujarat";	//7
+	}
+	if (capital=="Chandigarh")
+		{	
+	System.out.println(capital);
+		System.out.println("is the capital city of the state");
+		return "Haryana";	//8
+	}
+	if (capital=="shimla")
+		{	
+	System.out.println(capital);
+		System.out.println("is the capital city of the state");
+		return "Himachal Pradesh";	//9
+	}
+	if (capital=="Srinagar")
+		{	
+	System.out.println(capital);
+		System.out.println("is the capital city of the state");
+		return "jammu and kashmir";	//10
+	}
+if (capital=="Ranchi")
+		{	
+	System.out.println(capital);
+		System.out.println("is the capital city of the state");
+		return "Jharkhand";	//11
+	}	
+	if (capital=="Trivandrum")
+		{	
+	System.out.println(capital);
+		System.out.println("is the capital city of the state");
+		return "Kerala";	//12
+	}
+	if (capital=="Bhopal")
+		{	
+	System.out.println(capital);
+		System.out.println("is the capital city of the state");
+		return "Madhya Pradesh";	//13
+	}
+	if (capital=="Mumbai")
+		{	
+	System.out.println(capital);
+		System.out.println("is the capital city of the state");
+		return "Maharashtra";	//14
+	}
+	if (capital=="Imphal")
+		{	
+	System.out.println(capital);
+		System.out.println("is the capital city of the state");
+		return "Manipur";	//15
+	}
+	if (capital=="Shillong")
+		{	
+	System.out.println(capital);
+		System.out.println("is the capital city of the state");
+		return "Meghalaya";	//16
+	}
+	if (capital=="Aizawl")
+		{	
+	System.out.println(capital);
+		System.out.println("is the capital city of the state");
+		return "Mizoram";	//17
+	}
+	if (capital=="Kohima")
+	{
+	System.out.println(capital);
+		System.out.println("is the capital city of the state");
+		return "Nagaland";	//18
+	}
+	if (capital=="Bhubaneswar")
+	{
+	System.out.println(capital);
+		System.out.println("is the capital city of the state");
+		return "Odisha";	//19
+	}
+	
+	if (capital=="Chandigarh")
+	{	
+System.out.println(capital);
+		System.out.println("is the capital city of the state");
+		return "Punjab";	//20
+	}
+	if (capital=="Jaipur")
+		{	
+	System.out.println(capital);
+		System.out.println("is the capital city of the state");
+		return "Rajasthan";	//21
+	}
+	if (capital=="Gangtok")
+		{	
+	System.out.println(capital);
+		System.out.println("is the capital city of the state");
+		return "Sikkim";	//22
+	}
+	if (capital=="Chennai")
+		{	
+	System.out.println(capital);
+		System.out.println("is the capital city of the state");
+		return "Tamil Nadu";	//23
+	}
+	if (capital=="Hyderabad")
+		{	
+	System.out.println(capital);
+		System.out.println("is the capital city of the state");
+		return "Telangana";	//24
+	}
+	if (capital=="Agartala")
+		{	
+	System.out.println(capital);
+		System.out.println("is the capital city of the state");
+		return "Tripura";	//25
+	}
+	if (capital=="Lucknow")
+		{	
+	System.out.println(capital);
+		System.out.println("is the capital city of the state");
+		return "Uttar Pradesh";	//26
+	}
+	if (capital=="Dehradun")
+		{	
+	System.out.println(capital);
+		System.out.println("is the capital city of the state");
+		return "Uttarakhand";	//27
+	}
+	if (capital=="Kolkata")
+		{	
+	System.out.println(capital);
+		System.out.println("is the capital city of the state");
+		return "West Bengal";	//28
+	}
+	if (capital=="Port Blair")
+		{	
+	System.out.println(capital);
+		System.out.println("is the capital city of the Union Territory");
+		return "Andaman and Nicobar Islands";	//30
+	}
+	if (capital=="Silvassa")
+		{	
+	System.out.println(capital);
+		System.out.println("is the capital city of the Union Territory");
+		return "Dadra and Nagar Haveli";	//31
+	}
+	if (capital=="Daman")
+		{	
+	System.out.println(capital);
+		System.out.println("is the capital city of the Union Territory");
+		return "Daman and Diu";	//32
+	}
+	if (capital=="New Delhi")
+		{	
+	System.out.println(capital);
+		System.out.println("is the capital city of the Union Territory");
+		return "Delhi";	//33
+	}
+	if (capital=="Leh")
+		{	
+	System.out.println(capital);
+		System.out.println("is the capital city of the Union Territory");
+		return "Ladakh ";	//33
+	}
+	if (capital=="Kavaratti")
+		{	
+	System.out.println(capital);
+		System.out.println("is the capital city of the Union Territory");
+		return "Lakshadweep ";	//33
+	}
+	if (capital=="Pondicherry")
+		{	
+	System.out.println(capital);
+		System.out.println("is the capital city of the Union Territory");
+		return "Puducherry";	//33
+	}
+	
+	
+	
+	
+	return null;
+	}
+	
+	
+	
+	
+}
---
+++ CapitalByStateRunner.java
@@ -0,0 +1,11 @@
+class CapitalByStateRunner{
+public static void main (String [] racer){
+	
+	String ref=CapitalByState.getCapitalByState("Kolkata");
+	System.out.println(ref);
+	
+	
+	
+}
+
+}
