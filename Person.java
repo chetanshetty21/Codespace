d701c3edb527a5735528087ec11ad5260b21c036
unknown <chetansv.xworkz@gmail.com>
2022-09-18 22:46:51 +0530

    16 9 22

---
+++ Person.java
@@ -0,0 +1,42 @@
+class Person{
+static void work (String Company, double Salary){
+	
+		   System.out.println("DETAILS OF COMPANY");
+	    if(Company=="Infosys"&&Salary==4.5d)
+		{
+			System.out.println("COMPANY "+Company);
+
+			System.out.println("SALARY="+4.5d);
+		System.out.println("it's only for the fresher");
+		return;
+		}
+}
+		static void work (String Company, double Salary, String designation)
+		{
+			if(Company=="tcs"&&Salary==5.5d&&designation=="software engg")
+			{
+				System.out.println("COMPANY= "+Company);
+
+			System.out.println("SALARY="+4.5d);
+		
+		System.out.println(designation);
+System.out.println("it's only for the fresher");
+
+	
+		return;
+		}
+		}	
+	}
+		
+			
+			
+			
+			
+		
+		
+		
+   
+    
+   
+	
+	
---
+++ PersonRunner.java
@@ -0,0 +1,15 @@
+class PersonRunner{
+	public static void main(String [] args)
+	
+	{
+	Person.work (	"Infosys",4.5d);
+	Person.work (	"tcs",5.5d,"software engg");
+		
+		
+		
+		
+	}
+	
+	
+	
+}
