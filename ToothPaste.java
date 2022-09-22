a748a6c2efb40224c13389cedc1f37548c1969e0
unknown <chetansv.xworkz@gmail.com>
2022-09-22 08:05:07 +0530

    22 9 22

---
+++ ToothPaste.java
@@ -0,0 +1,152 @@
+ class ToothPaste{
+	 static String brand; 
+	 static int  price;
+	 static String size;
+	 static String flavour;
+	 static double mfgDate;  
+	 static long barCodeNo;
+	 static double expDate;
+     static String color;
+	 static long coustmerNo;
+	 static String warning;
+	 static String taste;
+	 static String uses;
+	 static boolean salt;
+	 static int weight;
+	 static String ingredients;
+	 
+	 static void setBrand(String names1)
+	 {
+		 brand=names1;		 	 
+	 }
+	 static void setPrice(int value1)
+	 {
+		price=value1;
+		
+	 }
+     static void setFlavour(String names2)
+	 {
+		 flavour=names2;		 	 
+	 }	 
+	 static void setSize(String value2)
+	 {
+		 size=value2;		 	 
+	 }	
+	 static void setMfgDate(double date1)
+	 {
+		 mfgDate=date1;		 	 
+	 }
+     static void setBarCodeNo(long code)
+	 {
+		 barCodeNo=code;
+		 
+	 } 	 
+	 static void setExpDate(double date2)
+	 {
+		 expDate=date2;
+	 }
+	 static void setColor(String banna)
+	 {
+		 color=banna;
+		 
+	 }
+	 static void setCoustemerNo(long no)
+	 {
+		 coustmerNo=no;
+	 }
+	 static void setWarning(String note)
+	 {
+	    warning=note;
+ }
+	 static void setTaste(String ruchi)
+	 {
+		 taste=ruchi;
+	 }
+	 static void setuser(String details)
+	 {
+		  uses=details;
+	 }
+	 static void ifSalt( boolean upu)
+	 {
+		 salt=upu;
+	 }
+	 static void setWeight(int grams)
+	 {
+		 weight=grams;
+	 }
+	 static void setIngredients(String items)
+	 {
+		 
+		 ingredients=items;
+	 }
+	 static String getBrand()
+	 {
+		 return brand;		 	 
+	 }
+	 static int getPrice()
+	 {
+		return price;
+		
+	 }
+     static String getFlavour()
+	 {
+		 return flavour;		 	 
+	 }	 
+	 static String getSize()
+	 {
+		 return size;		 	 
+	 }	
+	 static double getMfgDate()
+	 {
+		 return mfgDate;		 	 
+	 }
+     static long getBarCodeNo()
+	 {
+		 return barCodeNo;
+		 
+	 } 	 
+	 static double getExpDate()
+	 {
+		 return expDate;
+	 }
+	 static String getColor()
+	 {
+		return color;
+		 
+	 }
+	 static long getCoustemerNo()
+	 {
+		return coustmerNo;
+	 }
+	 static String getWarning()
+	 {
+	   return warning;
+ }
+	 static String getTaste()
+	 {
+		return taste;
+	 }
+	 static String  getuser()
+	 {
+		  return uses;
+	 }
+	 static boolean getSalt( )
+	 {
+		 return salt;
+	 }
+	 static int getWeight()
+	 {
+		 return  weight;
+	 }
+	 static String getIngredients()
+	 {
+		 
+		 return ingredients;
+	 }
+	 
+	 
+	 
+	 
+	 
+ }
+ 
---
+++ ToothPasteRunner.java
@@ -0,0 +1,102 @@
+class ToothPasteRunner{
+public static void main(String [] racer){
+	
+
+ ToothPaste. setBrand ("Colgate");
+    System. out.println("Colgate");
+	
+	ToothPaste. setPrice (120);
+    System. out.println("120");
+	
+	ToothPaste. setFlavour ("charcoal");
+    System. out.println("charcoal");
+	
+	ToothPaste. setSize ("small");
+    System. out.println("small");
+	
+	
+	ToothPaste. setMfgDate (21222d);
+    System. out.println("21222d");
+	
+	ToothPaste. setBarCodeNo (12789546l);
+    System. out.println("12789546l");
+	
+	ToothPaste. setExpDate (21223d);
+    System. out.println("21223d");
+	
+	ToothPaste. setColor ("black");
+    System. out.println("black");
+	
+	ToothPaste. setCoustemerNo (180078562212l);
+    System. out.println("180078562212l");
+	
+	ToothPaste. setWarning ("to keep ur teeth white do brush");
+    System. out.println("to keep ur teeth white do brush");
+	
+	ToothPaste. setTaste ("savi ruchi");
+    System. out.println("savi ruchi");
+	
+	ToothPaste. setuser ("chetan is the user");
+    System. out.println("chetan is the user");
+	
+	ToothPaste. ifSalt (true);
+    System. out.println("sappe");
+	
+	ToothPaste. setWeight (50);
+    System. out.println("50");
+	
+	ToothPaste. setIngredients("hydrogen");
+    System. out.println("hydrogen");   
+	
+	
+	
+	
+	String brand = ToothPaste.getBrand ();
+    System. out.println("Colgate");
+	
+	int  price= ToothPaste.getPrice ();
+    System. out.println("120");
+	
+	String flavour = ToothPaste.getFlavour ();
+    System. out.println("charcoal");
+	
+	 String size= ToothPaste.getSize ();
+    System. out.println("small");
+	
+	
+	double mfgDate=ToothPaste. getMfgDate ();
+    System. out.println("21222d");
+	
+	long barCodeNo= ToothPaste. getBarCodeNo ();
+    System. out.println("12789546l");
+	
+	double expDate=ToothPaste. getExpDate ();
+    System. out.println("21223d");
+	
+	String color=ToothPaste. getColor ();
+    System. out.println("black");
+	
+	long coustmerNo=ToothPaste. getCoustemerNo ();
+    System. out.println("180078562212l");
+	
+	String warning=ToothPaste. getWarning ();
+    System. out.println("to keep ur teeth white do brush");
+	
+	String taste=ToothPaste. getTaste ();
+    System. out.println("savi ruchi");
+	
+	String uses =ToothPaste. getuser ();
+    System. out.println("chetan is the user");
+	
+	 boolean salt=ToothPaste. getSalt ();
+    System. out.println("sappe");
+	
+	int weight=ToothPaste. getWeight ();
+    System. out.println("50");
+	
+	String ingredients=ToothPaste. getIngredients();
+    System. out.println("hydrogen");
+	
+	
+}
+}
